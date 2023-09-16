4cade5a60d6decec1bc3b6524e9540a0c24816e6
Thomy <126921685+Ywidow@users.noreply.github.com>
2023-09-15 22:09:58 -0300

    Delete BarberApi directory

--- BarberApi/src/Barber.Api/Models/TelephoneToReturnDto.cs
+++
@@ -1,7 +0,0 @@
-namespace Barber.Api.Entities;
-
-public class TelephoneToReturnDto{
-  public int Id { get; set; }
-  public string Number { get; set; } = string.Empty;
-  public TelephoneType Type { get; set; }
-}
